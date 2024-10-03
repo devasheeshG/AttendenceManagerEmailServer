1707d588b7de68287edcadc71f1d90838665a581
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-10-03 14:12:06 +0000

    init project

---
+++ app/config.py
@@ -0,0 +1,33 @@
+# Path: `app/env.py`
+# Description: This file contains code to load `.env` file and make a pydantic `BaseSettings` class which can be used to access environment variables in the application.
+
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # PostgreSQL Configuration
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+
+    @classmethod
+    def get_postgres_uri(cls) -> str:
+        return f"postgresql://{cls().POSTGRES_USER}:{cls().POSTGRES_PASSWORD}@{cls().POSTGRES_HOST}:{cls().POSTGRES_PORT}/{cls().POSTGRES_DB}"
+
+    # ACS Configuration
+    ACS_EMAIL: str
+    ACS_KEY: str
+    ACS_ENDPOINT: str
+    
+    # Application Configuration
+    UPDATE_INTERVAL: int = 600  # 10 minutes in seconds
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
